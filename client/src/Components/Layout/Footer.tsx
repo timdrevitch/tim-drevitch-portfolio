ef507e0a063f33ae9eb54a1b70ad3d2f94310f72
timdrevitch <timdrevitch@gmail.com>
2022-10-16 22:39:00 -0400

    🦵 Update footer

--- client/src/Components/Layout/Footer.tsx
+++ client/src/Components/Layout/Footer.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react"
+import { LinkItem } from "../../Styles/ContactStyles"
 import { FooterContainer } from "../../Styles/FooterStyles"
+import { SMListItem } from "../../Styles/SocialMediaStyles"
 
 const Footer: FC = (): JSX.Element => {
   const current: Date = new Date()
@@ -8,6 +10,18 @@ const Footer: FC = (): JSX.Element => {
   return (
     <FooterContainer>
       <div>© All Rights Reserved {thisYear} Tim Drevitch</div>
+      <SMListItem>
+        View the repository to this website at{" "}
+        <LinkItem
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/timdrevitch/tim-drevitch-portfolio"
+        >
+          github.com/timdrevitch/tim-drevitch-portfolio
+        </LinkItem>{" "}
+      </SMListItem>
+      <br />
+      <br />
       <br />
     </FooterContainer>
   )
